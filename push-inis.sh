8200c6aa848d0f564d5e91481323634f2d7664bd
Blair Trump <btrump@vexcelco.com>
2020-01-24 19:58:51 -0500

    .

--- push-inis.sh
+++ push-inis.sh
@@ -2,7 +2,7 @@
 
 SRC="Saeid"
 SERVER="test"
-CHARS=("Assel" "Cepi" "Beiye" "Ovvw" "Qaol" "Pusha" "Adverb" "Nmqi" "Vruq")
+CHARS=("Assel" "Cepi" "Beiye" "Ovvw" "Qaol" "Pusha" "Adverb" "Nmqi" "Vruq" "Sophomore" "Kcnalb")
 for char in "${CHARS[@]}"; do
 	cp UI_${SRC}_${SERVER}.ini UI_${char}_${SERVER}.ini
 done
